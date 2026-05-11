7f49d8f29a87215b1bbcde93b1264114842c4e3b
Taylor Finklea <taylor.finklea@gmail.com>
2026-05-11 12:58:29 -0500

    Update larkline to v0.15.0

--- Formula/larkline.rb
+++ Formula/larkline.rb
@@ -6,22 +6,22 @@
 class Larkline < Formula
   desc "The line to all your tools — a keyboard-driven terminal command palette"
   homepage "https://github.com/TaylorFinklea/larkline"
-  version "0.14.0"
+  version "0.15.0"
   license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/TaylorFinklea/larkline/releases/download/v#{version}/lark-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "966de9268daeb1844f756daaeb4d6048f0031815ad0643d3adbabf9bf55f8ef6"
+      sha256 "fb606cf5c6fd32f4e4605d204a12fb6881ab050e4e172800637de6c971f16ef9"
     else
       url "https://github.com/TaylorFinklea/larkline/releases/download/v#{version}/lark-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "a159aa2cce8a82ad1d0639dc8c9b15e148c3ca9ff0eabb7afe92d1b25177afef"
+      sha256 "e1e5cb65715e7e1476056e4a4c3b5d7cf46f0107651a29f650f7527d98f7797b"
     end
   end
 
   on_linux do
     url "https://github.com/TaylorFinklea/larkline/releases/download/v#{version}/lark-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "b034d3e2baf0473fae66c34e681faca963e1fecd0456dcab5e27b4ec60c2dac1"
+    sha256 "68bfb70bd314ceff4a948b297747d9a30a345db0a7452945ad975c6926af562b"
   end
 
   def install
