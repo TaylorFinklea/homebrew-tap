ade9fc2ebe75c59b3ea588d5c1b469410394249d
Taylor Finklea <taylor.finklea@gmail.com>
2026-05-12 09:51:31 -0500

    Bump LayerKeys cask to v0.2.6

--- Casks/layerkeys.rb
+++ Casks/layerkeys.rb
@@ -1,6 +1,6 @@
 cask "layerkeys" do
-  version "0.2.5"
-  sha256 "f885a8988e7b6448134d11c6267600c5b6e2b4d0d0f3f0eb7792debabe5c274b"
+  version "0.2.6"
+  sha256 "6cf58b8cb281d91aff8f4ef55e941b620acd7c2a57e3003238b1c5fbbd85e4be"
 
   url "https://github.com/TaylorFinklea/layerkeys/releases/download/v#{version}/LayerKeys.zip"
   name "LayerKeys"
