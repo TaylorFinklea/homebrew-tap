f06e7f878c180e30246db981933cee8d2a18fe3f
Taylor Finklea <taylor.finklea@gmail.com>
2026-05-20 19:30:25 -0500

    Bump vimkeys to 0.7.7

--- Casks/vimkeys.rb
+++ Casks/vimkeys.rb
@@ -1,6 +1,6 @@
 cask "vimkeys" do
-  version "0.7.6"
-  sha256 "26b612af6017084fe80e61d281d28d1274b4e86dfbb2ad16ce0f799477524f76"
+  version "0.7.7"
+  sha256 "1d92c597aaf6fe0f4f3e5eb64f8bf845fb3384f4024f0f5aa1970cde958ee038"
 
   url "https://github.com/TaylorFinklea/vimkeys/releases/download/v#{version}/VimKeys.zip"
   name "VimKeys"
